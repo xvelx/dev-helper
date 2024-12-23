672472712fb72de38bc0da8600bae4c6a70272b0
Suryavel TR <surya.is.vel@gmail.com>
2024-12-23 08:18:00 +0000

    Added script for adding alias

---
+++ Scripts/set-git-alias.sh
@@ -0,0 +1,30 @@
+git config --global alias.st "status"
+
+git config --global alias.fo "fetch origin"
+
+git config --global alias.por "pull origin --rebase"
+#
+git config --global alias.fp "push origin --force"
+
+git config --global alias.cp "cherry-pick"
+
+git config --global alias.br "branch"
+git config --global alias.brd "branch -d"
+
+git config --global alias.ch "checkout"
+git config --global alias.chb "checkout -b"
+
+git config --global alias.rbc "rebase --continue"
+
+git config --global alias.ss "stash save"
+git config --global alias.sa "stash apply"
+git config --global alias.sp "stash pop"
+git config --global alias.sd "stash delete"
+
+git config --global alias.aa "add ."
+
+git config --global alias.confg "config --global"
+git config --global alias.confl "config --local"
+
+git config --global alias.cm "commit -m"
+git config --global alias.cma "commit --amend"
